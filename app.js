7c8487103b5c337b741419491be335f01a225491
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-08 17:07:37 +0600

    u

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(helmet());
 app.use(mongoSanitize());
 
 app.use(cors());
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
--- src/models/BrandModel.js
+++ src/models/BrandModel.js
@@ -2,6 +2,7 @@ const mongoose= require("mongoose")
 const DataSchema = new mongoose.Schema({    
   brandName:{type:String,unique:true,required:true},
   brandImage:{type:String,required:true},
+  // category:{type:mongoose.Schema.Types.ObjectId,ref:"categories",required:true}
 },{
     
     timestamps:true,versionKey:false
