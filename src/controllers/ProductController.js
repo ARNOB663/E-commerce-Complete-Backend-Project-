61c45aa7a554f6124ba3ea590ff7c8c83c33bcd2
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-19 22:19:33 +0600

    iyky

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -12,26 +12,36 @@ const {
 
 
 exports.ProductBrandList= async (req , res)=>
-{
+{ 
+    let result = await BrandListService();
+    return res.status(200).json(result);
 
 }
 
 exports.ProductCategoryList= async (req , res)=>{
+    let result = await CategoryListService();
+    return res.status(200).json(result);
 
 
 }
 
 exports.ProductSliderList =  async (req , res)=>{
 
+    let result = await SliderListService();
+    return res.status(200).json(result);
     
 }
 
 exports.ProductListByBrand = async (req , res)=>{
+    let result = await ListByBrandService(req);
+    return res.status(200).json(result);
 
 
 }
 
 exports.ProductListByCategory = async (req , res)=>{
+    let result = await ListByCategoryService(req);
+    return res.status(200).json(result);
 
 
 }
--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -1,15 +1,17 @@
+const  mongoose = require('mongoose');
 const BrandModel=require('../models/BrandModel');
 const CategoryModel=require('../models/CategoryModel');
 const ProductSliderModel=require('../models/ProductSliderModel');
 const ProductModel=require('../models/ProductModel');
 const ProductDetailModel=require('../models/ProductDetailModel');
 const ProductReviewModel=require('../models/ReviewModel');
+const ObjectId = mongoose.Types.ObjectId;//imported mongoose object id to use it in the code
 
 const BrandListService = async () => {
     // Logic to get the list of product brands   
     try{
         let data = await BrandModel.find(); // get all the brands from the database
-        return {status:"Success",data:data}.toString() // return the data in string format 
+        return {status:"Success",data:data} // return the data in string format 
     }
     catch(err){
         return {status:"fail",data:err}.toString() // return the error in string format
@@ -20,13 +22,11 @@ const CategoryListService = async () => {
     try{
         let data = await CategoryModel.find(); // get all the categories from the database
         return {status:"Success",data:data}.toString() // return the data in string format
-         
     }
 catch(err){
     return {status:"fail",data:err}.toString() // return the error in string format
 }
 }
-
 
 const SliderListService = async () => {
    try{
@@ -40,11 +40,28 @@ const SliderListService = async () => {
 
 }
 
-const ListByBrandService = async () => {
-    
-}
+const ListByBrandService = async (req) => {
+    try {
+        let BrandID = ObjectId(req.params.BrandID);
+        let MatchStage = { $match: { BrandID: BrandID } };
+        let JoinWithBrandStage = { $lookup: { from: "brands", localField: "BrandID", foreignField: "_id", as: "brands" } }; // join with brand collection to get brand name and logo 
+        let JoinWithCategoryStage = { $lookup: { from: "categories", localField: "CategoryID", foreignField: "_id", as: "categories" } }; // join with category collection to get category name
 
-const ListByCategoryService = async () => {
+        let data = await ProductModel.aggregate([
+            MatchStage,
+            JoinWithBrandStage,
+            JoinWithCategoryStage
+        ]);
+
+        console.log(data); // Log the data to debug
+
+        return { status: "Success", data: data }; // return the data as an object
+    } catch (err) {
+        return { status: "fail", data: err }; // return the error as an object
+    }
+};
+
+const ListByCategoryService = async (req) => {
 
 }
 
